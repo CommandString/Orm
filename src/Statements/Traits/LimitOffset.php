36b9b879f56d60f51adc721cbd1ab6a37bedffba
Command_String <bmxmikey2@gmail.com>
2022-11-25 09:52:56 -0500

    Added Limit & Offset Trait

--- src/Statements/Traits/LimitOffset.php
+++ src/Statements/Traits/LimitOffset.php
@@ -2,8 +2,9 @@
 
 namespace CommandString\Orm\Statements\Traits;
 
-trait Limit {
+trait LimitOffset {
     private int $limit = 0;
+    private int $offset = -1;
 
     public function limit(int $limit): self
     {
@@ -15,6 +16,19 @@ trait Limit {
     private function buildLimit(string &$query) {
         if ($this->limit > 0) {
             $query .= " LIMIT {$this->limit}";
+        }
+    }
+
+    public function offset(int $offset): self
+    {
+        $this->offset = $offset;
+
+        return $this;
+    }
+
+    public function buildOffset(&$query) {
+        if ($this->offset > -1) {
+            $query .= " OFFSET {$this->offset}";
         }
     }
 }
