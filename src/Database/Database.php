6b455e0eb89ee6110f9f7786851ea90b3072dcaa
Command_String <bmxmikey2@gmail.com>
2022-11-27 18:12:53 -0500

    Added name property and fix bug with buildTables

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -13,18 +13,26 @@ use stdClass;
 abstract class Database {
     public readonly Driver $driver;
     public readonly stdClass $tables;
+    public readonly string $name;
 
     public function __construct(Driver $driver) {
         $this->driver = NeedPdoDriver::checkDriver($driver);
         $this->tables = new stdClass;
+        $this->name = $this->getName();
         $this->buildTables();
+    }    
+
+    private function getName(): string
+    {   
+        $parts = explode("\\", get_called_class());
+        return $parts[count($parts)-1];
     }
 
     private function buildTables() {
         $tables = (new ReflectionClass(get_called_class()))->getConstants();
 
 		foreach ($tables as $tableName) {
-			$className = '\\'.__NAMESPACE__.'\\'.ucfirst($tableName);
+			$className = "\\".str_replace("$this->name", "", get_called_class()).ucfirst($tableName);
 			$this->tables->$tableName = new $className($this->driver);
 		}
     }
