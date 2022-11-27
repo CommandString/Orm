b182e80d101b303668530c1835dcab82784c70ea
Command_String <bmxmikey2@gmail.com>
2022-11-27 16:52:07 -0500

    NeedPdoDriver::checkDriver() method added

--- src/Traits/NeedPdoDriver.php
+++ src/Traits/NeedPdoDriver.php
@@ -8,10 +8,15 @@ trait NeedPdoDriver {
     public readonly Driver $driver;
 
     public function __construct(Driver $driver) {
+        $this->driver = self::checkDriver($driver);
+    }
+
+    public static function checkDriver(Driver $driver): Driver
+    {
         if (!isset($driver->driver)) {
             $driver->connect();
         }
 
-        $this->driver = $driver;
+        return $driver;
     }
 }
