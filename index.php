4efcfc6d815e197084a2782c440ee484f8bf5153
Command_String <bmxmikey2@gmail.com>
2022-11-27 19:22:20 -0500

    new test code

--- index.php
+++ index.php
@@ -2,25 +2,39 @@
 
 require_once __DIR__."/vendor/autoload.php";
 
-use CommandString\Orm\Database\City;
-use CommandString\Orm\Database\World;
-use CommandString\Orm\Orm;
+use CommandString\Orm\Builder;
+use CommandString\Orm\Database\Test\Classes;
+use CommandString\Orm\Database\Test\Teachers;
+use CommandString\Orm\Database\Test\Testing;
+use CommandString\Orm\Operators;
 use CommandString\Pdo\Driver;
 
 $driver = (new Driver())
 	->withUsername("admin")
 	->withPassword("password")
-	->withDatabase("world")
+	->withDatabase("testing")
 	->withHost("127.0.0.1")
 	->connect()
 ;
 
-$orm = new Orm($driver);
+$builder = new Builder($driver);
 
-$orm->build([
-    "output-dir" => __DIR__."/src/Database",
-    "namespace" => "CommandString\\Orm\\Database",
-	"database" => "World"
-]);
+$builder
+	->setOption(Builder::NAMESPACE, "CommandString\\Orm\\Database\\Test")
+	->setOption(Builder::OUTPUT_DIR, __DIR__."/src/Database/Test")
+	->database("testing")
+;
 
-$database = (new World($driver));
+$database = new Testing($driver);
+
+$results = $database->tables->classes
+	->select()
+	->columns([Teachers::F_NAME => "first_name"], [Teachers::L_NAME => "last_name"], Classes::NAME)
+	->leftJoin(Testing::TEACHERS)
+	->on(Teachers::ID, Classes::TEACHER_ID)
+	->where(Classes::ID, Operators::EQUAL_TO, 1)
+;
+
+echo $results;
+
+var_dump($results->execute()->fetch(PDO::FETCH_OBJ));
